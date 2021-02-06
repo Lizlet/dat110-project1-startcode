38f5d4f9504c8d3941f9a9286eb0b9fbbf0a41fb
Sondre <songro93@gmail.com>
2021-02-06 21:17:49 +0100

    Completed DisplayDevice

--- dat110-project1/src/no/hvl/dat110/system/display/DisplayDevice.java
+++ dat110-project1/src/no/hvl/dat110/system/display/DisplayDevice.java
@@ -15,9 +15,21 @@ public class DisplayDevice {
 		// implement the operation of the display RPC server
 		// see how this is done for the sensor RPC server in SensorDevice
 		
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
-		}
+		//=================================================================
+		
+		DisplayImpl displayImpl = new DisplayImpl();
+		RPCServer displayServer = new RPCServer(Common.DISPLAYPORT);
+		displayServer.register(1, displayImpl);
+		
+		displayServer.run();
+		displayServer.stop();
+		
+		
+		//=================================================================
+		
+//		if (true) {
+//			throw new UnsupportedOperationException(TODO.method());
+//		}
 		
 		System.out.println("Display server stopping ...");
 		
